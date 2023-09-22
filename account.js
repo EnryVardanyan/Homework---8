7900331530dc6fcd031897968fc8fbfe56fe28c7
Enry Vardanyan <Vardanyan.enry7@gmail.com>
2023-09-22 14:08:33 -0700

    second task (Account) transferTo, identifyAccounts methods created

--- account.js
+++ account.js
@@ -7,10 +7,6 @@ class Account {
       this.#balance = balance;
     }
   
-    static uniqueIdCreation() {
-      return Math.random().toString().slice(2, 12)
-    }
-  
     get balance() {
       return this.#balance
     }
@@ -37,12 +33,29 @@ class Account {
             throw new Error("Incorrect Input")
           }
     }
+    transferTo(account, amount){
+        if(amount <= this.#balance && typeof amount === "number" && amount > 0){
+            this.#balance -= amount
+            account.credit(amount)
+        } else throw new Error("Incorrect Input")
+    }
+    static uniqueIdCreation() {
+        return Math.random().toString().slice(2, 12)
+      }
+    static identifyAccounts(acc1, acc2){
+        return acc1.id === acc2.id
+    }
   }
   
-  const account1 = new Account("barev", 100000);
+  const account1 = new Account("MyAccount", 100000);
+  const account2 = new Account("MySecondAccount", 1000)
   account1.balance = 1000
   console.log(account1.balance)
   account1.credit(1000)
   console.log(account1.balance)
   account1.debit(1000)
   console.log(account1.balance)
+  account1.transferTo(account2, 1000)
+  console.log(account2.balance, account2.name)
+  console.log(Account.identifyAccounts(account1, account1))
+  console.log(Account.identifyAccounts(account1, account2))
