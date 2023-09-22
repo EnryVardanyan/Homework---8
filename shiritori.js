cf69a48a237890a6d74676549f5512eeb19ffee2
Enry Vardanyan <Vardanyan.enry7@gmail.com>
2023-09-22 10:23:20 -0700

    first task(Shiritori) method play created

---
+++ shiritori.js
@@ -0,0 +1,23 @@
+class Shiritori{
+    constructor(){
+        this.words = []
+        this.game_over = false
+    }
+    play(word){
+        if(!this.words.length){
+            this.words.push(word)
+            return this.words
+        }
+        const lastItem = this.words[this.words.length - 1]
+        if(lastItem.charAt(lastItem.length - 1).toLowerCase() === word[0].toLowerCase() && !(this.words.includes(word))){
+            this.words.push(word)
+            return this.words
+        }   
+        this.game_over = true
+        return "game over"
+    }
+}
+const shiritori = new Shiritori()
+console.log(shiritori.play("hello"))
+console.log(shiritori.play("Ohio"))
+console.log(shiritori.play("Ohio"))
